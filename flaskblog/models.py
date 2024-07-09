632dfa7002f744ef7fa2726d0c6c2a568cacfd75
Franco Viccia <francoviccia@gmail.com>
2024-07-09 17:12:41 +0200

    Add user account and profile picture management

--- flaskblog/models.py
+++ flaskblog/models.py
@@ -15,7 +15,7 @@ class User(db.Model, UserMixin):
     # Nullable is in False to enforce the existence of the field.
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    image_file = db.Column(db.String(20), nullable=False, default="default.jpeg")
+    image_file = db.Column(db.String(20), nullable=False, default="default.jpg")
     password = db.Column(db.String(60), nullable=False)
     """
     Setting a relationship to the Post class, 
